7bc06f5d70d0b9fd5ad4705de478284f6d8ce7b5
ivanFlor3s <ifloresmanc96@gmail.com>
2023-07-03 23:35:06 -0300

    Cambios en etidades + indexes

--- Turnos/Models/Turno.cs
+++ Turnos/Models/Turno.cs
@@ -8,11 +8,11 @@ namespace Turnos.Models
         public int IdTurno { get; set; }
         [Required]
         public DateTime Fecha { get; set; }
-        [Required(ErrorMessage ="Erroraso")]
+        [Required(ErrorMessage ="Falta el Medico")]
         public int MedicoId { get; set; }
         //PROP DE NAVEGACION
         public Medico? Medico { get; set; } = null!;
-        [Required]
+        [Required(ErrorMessage = "Falta el Paciente")]
         public int PacienteId { get; set; }
         //PROP DE NAVEGACION
         public Paciente? Paciente { get; set; } = null!;
